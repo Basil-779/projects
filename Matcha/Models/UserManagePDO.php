d5ea5aac56c856b2280292f6f5abaeb7e330627b
Basil <basil@basil-TM1701>
2019-12-26 12:17:30 +0300

    added matcha

---
+++ Matcha/Models/UserManagePDO.php
@@ -0,0 +1,23 @@
+<?php
+namespace Models;
+use User;
+use \PDO;
+
+class UserManagePDO extends UserManager
+{
+    /*
+    P
+    L
+    A
+    C
+    E
+    H
+    O
+    L
+    D
+    E
+    R
+    */
+}
+
+?>
