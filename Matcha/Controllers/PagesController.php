8f29027ff1d46e6a01b2c61231daf2251a694319
Basil-779 <basilgoremykin@mail.ru>
2020-01-26 20:39:12 +0300

    added LogIn

--- Matcha/Controllers/PagesController.php
+++ Matcha/Controllers/PagesController.php
@@ -12,11 +12,11 @@ class PagesController extends Controller
     {
         if (!Validator::isConnected())
         {
-        return $this->render($response, /**SIGN UP PAGE */);
+        return $this->render($response, 'SIGN UP PAGE HERE');
         }
         else
         {
-            return $this->redirect($response, /**HOME PAGE */, 200);
+            return $this->redirect($response, 'home', 200);
         }
     }
     public function postSignUp($request, $response)
@@ -94,12 +94,72 @@ class PagesController extends Controller
         {
             $this->flash('Something wend wrong', 'error');
             $this->flash($errors, 'errors');
-            return $this->redirect($response, /**HOME PAGE */, 302);
+            return $this->redirect($response, 'HOME PAGE HERE', 302);
         }
 
         $this->flash('A confirmation email was sent to you', 'info');
-        return $this->redirect($response, /**LOGIN PAGE */, 200);
+        return $this->redirect($response, 'LOGIN PAGE HERE', 200);
     }
+
+    public function getLogIn($request, $response)
+    {
+        $user = '';
+
+        if (!Validator::isConnected())
+        {
+            $UserManagerPDO = new UserManagerPDO($this->db);
+            return $this->render($response, 'LOGIN PAGE HERE');
+        }
+        else 
+        {
+            return $this->redirect($response, 'home', 200);
+        }
+    }
+    
+    public function postLogIn($request, $response)
+    {
+        $errors = [];
+        $login = $request->getParam('login');
+        $password = $request->getParam('password');
+
+        if (Validator::loginCheck($login, $this->db))
+        {
+            if (Validator::isActive($login, $this->db))
+            {
+                if (Validator::passwordLogin($login, $password, $this->db))
+                {
+                    $UserManagerPDO = new UserManagerPDO($this->db);
+                    $id = $UserManagerPDO->getIdFromLogin($login);
+                    $user = $UserManagerPDO->getUnique($id);
+
+                    $_SESSION['id'] = serialize($id);
+                    setcookie("matcha_cookie", $_SESSION['id'], time() + 36000, "/");
+                }
+                else 
+                {
+                    $errors['passwod'] = 'Wrong password';
+                }
+            }
+            else 
+            {
+                $errors['login'] = 'Account is not activated';
+            } 
+        }
+        else 
+        {
+            $errors['login'] = 'This username doesnt exists';
+        }
+
+        if (!empty($errors))
+        {
+            $this->flash('Fields are not fullfilled correctly', 'error');
+            $this->flash($errors, 'errors');
+            return $this->redirect($response, 'auth.login', 302); /**AUTH.LOGIN - NAMED BY TWIG BEFORE */
+        }
+
+        return $this->redirect($response, 'home', 200);
+    }
+
 }
 
 ?>
